b8b24a5582bf37a0f0baf6cdb8621fba57931b9b
Monika Staciwa <staciwa@uni-hildesheim.de>
2023-08-21 21:00:50 +0200

    coordinate update is ok

--- program/state_tracker_test.py
+++ program/state_tracker_test.py
@@ -2,7 +2,7 @@ import unittest
 import state_tracker
 import command
 
-
+"""
 class TestPositionSetting(unittest.TestCase):
     def runTest(self):
         pos = [100,100,0,0]
@@ -10,18 +10,50 @@ class TestPositionSetting(unittest.TestCase):
         self.assertEqual(rectangle.pos, pos, "incorrect position")
 
 
+
+class Testing(unittest.TestCase):
+    def test_string(self):
+        a = 'some'
+        b = 'some'
+        self.assertEqual(a, b)
+
+    def test_boolean(self):
+        a = True
+        b = True
+        self.assertEqual(a, b)
+"""
+
+
+
+
 class TestUpdatingCoordinates(unittest.TestCase):
     def runTest(self):
         # setup
-        pos = [100,100,0,0]
+        pos = [100,100,0,270]
         state = state_tracker.State(pos)
-        cmd = command.Command("up", 20)
+        cmd = command.Command("cw", 180)
         state.update(cmd)
-        updated_pos = [100, 100, 20, 0]
+        updated_pos = [100, 100, 0, 90]
+
+        # exec
+        self.assertEqual(state.pos, updated_pos, "incorrect position update with cmd \"up\"")
+
+    def runTest2(self):
+        # setup
+        pos = [100,100,0,90]
+        state = state_tracker.State(pos)
+        cmd = command.Command("cw", 180)
+        state.update(cmd)
+        updated_pos = [100, 100, 0, 270]
 
         # exec
         self.assertEqual(state.pos, updated_pos, "incorrect position update with cmd \"up\"")
 
 
 
-unittest.main()
+
+
+
+
+if __name__ == '__main__':
+    unittest.main()
