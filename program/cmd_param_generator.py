192d46206bd8690943ab5f19152c8146254ac31e
Monika Staciwa <staciwa@uni-hildesheim.de>
2023-08-21 10:51:54 +0200

    cmd generator implemented

--- program/cmd_param_generator.py
+++ program/cmd_param_generator.py
@@ -4,16 +4,16 @@ import logging
 root = logging.getLogger()
 root.setLevel(logging.DEBUG)
 
-CMDS = [["takeoff", None],
-        ["land", None],
-        ["up", "cm"],
-        ["down", "cm"],
-        ["forward", "cm"],
-        ["back", "cm"],
-        ["left", "cm"],
-        ["right", "cm"],
-        ["cw", "degrees"],    # rotate clockwise
-        ["ccw", "degrees"]]   # rotate counter-clockwise
+CMDS = [#["takeoff", None],
+        #["land", None],
+        ["up", [20]],
+        ["down", [20]],
+        ["forward", [20, 40, 60]],
+        ["back", [20, 40, 60]],
+        ["left", [20, 40, 60]],
+        ["right", [20, 40, 60]],
+        ["cw", [90, 180]],    # rotate clockwise
+        ["ccw", [90, 180]]]   # rotate counter-clockwise
 
 
 class command:
@@ -22,17 +22,23 @@ class command:
         self.param = param
 
 
-def get_param(cmd_name, oaram_type):
-    result = 0
-
+def get_param(cmd_index):
+    result = None
+    logging.debug(f'[cmd_param_gen | get_param] Looking for the parameter for the command \"{CMDS[cmd_index][0]}\".')
+    if (CMDS[cmd_index][1]):
+        logging.debug(f'[cmd_param_gen | get_param] Possible parameters {CMDS[cmd_index][1]}')
+        param_index = rd.randint(0, len(CMDS[cmd_index][1]) - 1)
+        result = CMDS[cmd_index][1][param_index]
+        logging.debug(f'[cmd_param_gen | get_param] Choosen parameter is: {result}')
+    else:
+        logging.debug(f'[cmd_param_gen | get_param] This command does not required any parameters.')
     return result
 
 
 def get_cmd():
     cmd_index = rd.randint(0, len(CMDS) - 1)
-    parameter = 0
+    parameter = get_param(cmd_index)
     cmd = command(CMDS[cmd_index][0], param=parameter)
-    print(str(cmd_index))
     logging.debug("[cmd_param_gen | get_cmd] Generated command \"" + cmd.name + "\" with parameter " + str(cmd.param))
 
 
